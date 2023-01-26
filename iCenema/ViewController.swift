9ac38a5d917eafcc4377446e040388c090779d2c
Ahmed Yamany <yamany2862016@gmail.com>
2023-01-27 01:33:09 +0200

    Initial Commit

---
+++ iCenema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCenema
+//
+//  Created by Ahmed Yamany on 27/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
